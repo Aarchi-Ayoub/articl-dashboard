b3180f0dc3716f581b6e386e8b31cc8b2b4bb378
Ayoub Aarchi <ayoubaa084@gmail.com>
2021-06-15 19:18:27 +0100

    login prob

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { Admin, Resource } from "react-admin";
+import simpleRestProvider from "ra-data-simple-rest";
+import authProvider from "./auth/authProvider";
+import Dashboard from "./pages/Dashboard";
+import ListUsers from "./users/ListUsers";
 const App = () => {
-  return <div></div>;
+  const dataProvider = simpleRestProvider("http://192.168.11.104:8443/api/v1");
+  return (
+    <Admin
+      dashboard={Dashboard}
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+    >
+      <Resource name="users" list={ListUsers} />
+    </Admin>
+  );
 };
 
 export default App;
---
+++ src/users/ListUsers.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ListUsers = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quis
+      similique expedita incidunt reprehenderit dolorum veritatis eligendi
+      libero fugiat et blanditiis laboriosam aliquam velit numquam molestias
+      autem obcaecati, ut odio.
+    </div>
+  );
+};
+
+export default ListUsers;
